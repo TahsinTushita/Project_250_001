02d528ea63caa992326bc264e213c0ba4a5ffe64
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-01-11 22:32:41 +0600

    updated

--- app/src/main/java/com/sust/project_250_001/ReviewAdapter.java
+++ app/src/main/java/com/sust/project_250_001/ReviewAdapter.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 
 public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewHolder> {
 
-    private ImageView cover;
     private TextView bookTitle;
     private TextView userName;
     private TextView reviewText;
@@ -26,16 +25,15 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewHold
     public class ReviewHolder extends RecyclerView.ViewHolder {
         public ReviewHolder(@NonNull View itemView) {
             super(itemView);
-            cover = itemView.findViewById(R.id.bookCover);
             bookTitle = itemView.findViewById(R.id.bookTitle);
             userName = itemView.findViewById(R.id.userName);
             reviewText = itemView.findViewById(R.id.reviewText);
+            reviewText.setMaxLines(5);
         }
         public void setDetails(Review review) {
-            Picasso.get().load(review.getCoverUrl()).into(cover);
             bookTitle.setText(review.getBookTitle());
-            userName.setText(review.getUserName());
             reviewText.setText(review.getReviewText());
+            userName.setText(review.getUserName());
         }
     }
 
