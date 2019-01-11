2c33b9e75a279debb257cd075ba88a7fec4f2cc7
Tushita(2016831026) <kazi26@student.sust.edu>
2019-01-11 16:17:09 +0600

    added map

--- app/src/main/java/com/sust/project_250_001/Profile.java
+++ app/src/main/java/com/sust/project_250_001/Profile.java
@@ -1,4 +1,13 @@
 package com.sust.project_250_001;
 
-public class Profile {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
 }
