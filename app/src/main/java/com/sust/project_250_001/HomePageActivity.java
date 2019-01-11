25951268b94299ca8f1954dcb18b39fb7e8d3e68
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-01-11 18:24:47 +0600

    upd

--- app/src/main/java/com/sust/project_250_001/HomePageActivity.java
+++ app/src/main/java/com/sust/project_250_001/HomePageActivity.java
@@ -9,8 +9,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,6 +39,7 @@ public class HomePageActivity extends AppCompatActivity {
     private ArrayList<Book> bookArrayList;
     private ArrayList<Review> reviewArrayList;
     private DatabaseReference database,reviewDatabase;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +51,8 @@ public class HomePageActivity extends AppCompatActivity {
         bookArrayList = new ArrayList<>();
         adapter = new BookAdapter(this,bookArrayList,listener);
         recyclerView.setAdapter(adapter);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         //Recent Reviews
         reviewView = findViewById(R.id.reviewView);
@@ -166,7 +171,20 @@ public class HomePageActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.idLogout:
+                firebaseAuth.signOut();
+                if(firebaseAuth.getCurrentUser() == null)
+                startActivity(new Intent(HomePageActivity.this,LoginActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void createListData() {
