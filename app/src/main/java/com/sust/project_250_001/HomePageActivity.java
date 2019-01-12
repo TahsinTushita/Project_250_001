8359c02d38b75c0361715410516b882b7c474cfd
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-01-12 15:45:30 +0600

    Updated

--- app/src/main/java/com/sust/project_250_001/HomePageActivity.java
+++ app/src/main/java/com/sust/project_250_001/HomePageActivity.java
@@ -14,7 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -261,8 +261,8 @@ public class HomePageActivity extends AppCompatActivity implements NavigationVie
                 break;
 
         }
-
-
+        startActivity(intent);
         return false;
     }
+
 }
