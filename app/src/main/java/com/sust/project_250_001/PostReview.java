f22f4fd9564934b3ebc28dbdedc2174253806976
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-01-12 15:28:34 +0600

    Updated

--- app/src/main/java/com/sust/project_250_001/PostReview.java
+++ app/src/main/java/com/sust/project_250_001/PostReview.java
@@ -1,7 +1,6 @@
 package com.sust.project_250_001;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
