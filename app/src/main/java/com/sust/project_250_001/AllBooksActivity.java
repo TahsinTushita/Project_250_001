401ad9aa4b613340845edc07082b47983df7b311
Tushita(2016831026) <kazi26@student.sust.edu>
2019-01-24 19:57:44 +0600

    update

--- app/src/main/java/com/sust/project_250_001/AllBooksActivity.java
+++ app/src/main/java/com/sust/project_250_001/AllBooksActivity.java
@@ -1,12 +1,26 @@
 package com.sust.project_250_001;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class AllBooksActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +30,7 @@ public class AllBooksActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("All Books");
+
+
     }
 }
