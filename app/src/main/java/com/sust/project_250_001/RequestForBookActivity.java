6df66a4b582ce9140494b360635a3dc55ed8cde8
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-01-24 18:41:14 +0600

    Updated

---
+++ app/src/main/java/com/sust/project_250_001/RequestForBookActivity.java
@@ -0,0 +1,13 @@
+package com.sust.project_250_001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestForBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_for_book);
+    }
+}
