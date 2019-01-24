dedc83fbc90484acec7fe2fc70cebdcd51d9ee79
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-01-24 19:13:26 +0600

    Updated

--- app/src/main/java/com/sust/project_250_001/HomePageActivity.java
+++ app/src/main/java/com/sust/project_250_001/HomePageActivity.java
@@ -5,6 +5,7 @@ import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,6 +19,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -206,6 +208,7 @@ public class HomePageActivity extends AppCompatActivity implements NavigationVie
 
             }
         });
+        Snackbar.make(findViewById(android.R.id.content),"Welcome " + LoginActivity.user.toUpperCase(),Snackbar.LENGTH_LONG).show();
     }
 
         ValueEventListener reviewValueEventListener = new ValueEventListener() {
--- app/src/main/java/com/sust/project_250_001/RequestForBookActivity.java
+++ app/src/main/java/com/sust/project_250_001/RequestForBookActivity.java
@@ -1,12 +1,22 @@
 package com.sust.project_250_001;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RequestForBookActivity extends AppCompatActivity {
-
     private Toolbar toolbar;
+    private DatabaseReference database;
+
+    private EditText TitleText,AuthorText;
+    private Button SubmitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,28 @@ public class RequestForBookActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Send Request");
+
+        database = FirebaseDatabase.getInstance().getReference("Requests");
+
+        TitleText = findViewById(R.id.idBookName);
+        AuthorText = findViewById(R.id.idBookAuthor);
+        SubmitBtn = findViewById(R.id.idBookReq);
+
+        SubmitBtn.setOnClickListener(listener);
     }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String title,author;
+            title = TitleText.getText().toString().trim();
+            author = AuthorText.getText().toString().trim();
+            if(title.isEmpty()==false) {
+                database.push();
+                database.child("title").setValue(title);
+                database.child("author").setValue(author);
+            } else
+                Snackbar.make(view.getRootView(),"Please specify a valid book name",Snackbar.LENGTH_LONG).show();
+        }
+    };
 }
