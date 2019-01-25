78c6e72601af4ff12d5900d992ed1ceb88aa2f92
Ahsan Aziz Ishan <ishan.bn14@gmail.com>
2019-01-25 18:28:05 +0600

    Updated

--- app/src/main/java/com/sust/project_250_001/UpdateProfile.java
+++ app/src/main/java/com/sust/project_250_001/UpdateProfile.java
@@ -1,5 +1,6 @@
 package com.sust.project_250_001;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +12,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 
 public class UpdateProfile extends AppCompatActivity {
@@ -49,27 +52,53 @@ public class UpdateProfile extends AppCompatActivity {
         pwd = fieldPwd.getText().toString();
         name = fieldName.getText().toString();
         address = fieldAdress.getText().toString();
-
         boolean err = false;
-        if(!email.isEmpty()) {
-            database.child("email").setValue(email);
-        }
-        if(pwd.length() > 5) {
-            if (!pwd.isEmpty())
-            firebaseAuth.getCurrentUser().updatePassword(pwd);
-        } else {
-            Toast.makeText(this,"Unable to update password",Toast.LENGTH_LONG).show();
-            err = true;
-        }
+        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> queries = new ArrayList<>();
 
-        if(!address.isEmpty()) {
-            database.child("address").setValue(address);
-        }
+        queries.add("Name ");
         if(!name.isEmpty()) {
             database.child("name").setValue(name);
+            result.add("was changed!");
+        } else {
+            result.add("was not changed.");
         }
-        if (!err)
-        onBackPressed();
+        queries.add("Email ");
+        if(!email.isEmpty()) {
+            database.child("email").setValue(email);
+            result.add("was changed!");
+        } else {
+            result.add("was not changed.");
+        }
+        queries.add("Password ");
+        if(!pwd.isEmpty()) {
+            if (pwd.length() > 5){
+            firebaseAuth.getCurrentUser().updatePassword(pwd);
+            result.add("was changed!");}
+             else if(pwd.length() < 5){
+            err=true;
+            Toast.makeText(this,"Unable to update password",Toast.LENGTH_LONG).show();
+        } }
+        else {
+            result.add("was not changed.");
+        }
 
+        queries.add("Address ");
+        if(!address.isEmpty()) {
+            database.child("address").setValue(address);
+        result.add("was changed!");
+        } else {
+            result.add("was not changed.");
+        }
+
+        String st="";
+        for(int i=0;i<queries.size();++i) {
+            st = st.concat (queries.get(i)+result.get(i)+"\n");
+        }
+
+        Snackbar.make(view.getRootView(),st,Snackbar.LENGTH_LONG).show();
+        if(!err){
+//            onBackPressed();
+        }
     }
 }
